683c581bb385f7fc822cbcb7d56c0a38c1c24774
adamcallison <callison.adam@gmail.com>
2023-03-20 17:59:19 +0000

    add problem 12

--- projecteuler/solutions/problem11.py
+++ projecteuler/solutions/problem11.py
@@ -55,12 +55,6 @@ if __name__ == '__main__':
         '20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54',
         '01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48'
     ]
-    #gridstr = [
-    #    '01 00 00 01',
-    #    '00 04 00 02',
-    #    '03 03 03 01',
-    #    '00 00 00 04'
-    #]
     grid = [[int(y) for y in x.split()] for x in gridstr]
     print(greatest_product_of_n_adjacent(n, grid))
 
